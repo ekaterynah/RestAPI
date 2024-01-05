dff7ffedbd3e737216fafa7b11cfc371deacae59
ekaterynah <katyagumeniuk12@gmail.com>
2024-01-06 00:17:03 +0200

    Added a POST request model

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const products = require('../data/products.json');
+const path = require('path')
+const uuid= require('uuid');
+const uuid4 = uuid.v4();
+
+const { addToDataFile } = require('../addToDataFile');
 
 function findAllProducts(){
     return new Promise((resolve, reject) => {
@@ -15,7 +20,20 @@ function findProductById(id){
     });
 }
 
+function createNewProduct(product){
+    return new Promise((resolve, reject) => {
+        const newProduct = {id: uuid4, ...product};
+
+        products.push(newProduct);
+
+        const filePath = path.join(__dirname, '../data', 'products.json');
+        addToDataFile(filePath, products);
+        resolve(newProduct)
+    });
+}
+
 module.exports = {
     findAllProducts,
-    findProductById
+    findProductById,
+    createNewProduct
 }
